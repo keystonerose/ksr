c173ac49366d16848179c1148388f6dc1540e65f
James Dent <semajdent@gmail.com>
2017-05-28 13:14:18 +1200

    ~ added flag_product class

--- include/ksr/flag_product.hpp
+++ include/ksr/flag_product.hpp
@@ -3,31 +3,56 @@
 
 #include "type_util.hpp"
 
-/// Given an scoped enumeration type `flags_t`, defines non-throwing, `constexpr` implementations of
-/// the bitwise operators (that is, `&`, `|`, `^`, `~`, `&=`, `|=` and `^=`), each of which returns
-/// another `flags_t` object.
+#include <tuple>
 
-#define KSR_DEFINE_FLAGS(flags_t) \
-    [[maybe_unused]] constexpr auto operator&(const flags_t lhs, const flags_t rhs) noexcept -> flags_t { \
-        return static_cast<flags_t>(under_cast(lhs) & under_cast(rhs)); \
+/// Given an scoped enumeration type `enum_t`, defines non-throwing, `constexpr` implementations of
+/// the bitwise operators (that is, `&`, `|`, `^`, `~`, `&=`, `|=` and `^=`), each of which returns
+/// another `enum_t` object.
+
+#define KSR_DEFINE_BIT_OPERATIONS(enum_t) \
+    [[maybe_unused]] constexpr auto operator&(const enum_t lhs, const enum_t rhs) noexcept -> enum_t { \
+        return static_cast<enum_t>(under_cast(lhs) & under_cast(rhs)); \
     } \
-    [[maybe_unused]] constexpr auto operator|(const flags_t lhs, const flags_t rhs) noexcept -> flags_t { \
-        return static_cast<flags_t>(under_cast(lhs) | under_cast(rhs)); \
+    [[maybe_unused]] constexpr auto operator|(const enum_t lhs, const enum_t rhs) noexcept -> enum_t { \
+        return static_cast<enum_t>(under_cast(lhs) | under_cast(rhs)); \
     } \
-    [[maybe_unused]] constexpr auto operator^(const flags_t lhs, const flags_t rhs) noexcept -> flags_t { \
-        return static_cast<flags_t>(under_cast(lhs) ^ under_cast(rhs)); \
+    [[maybe_unused]] constexpr auto operator^(const enum_t lhs, const enum_t rhs) noexcept -> enum_t { \
+        return static_cast<enum_t>(under_cast(lhs) ^ under_cast(rhs)); \
     } \
-    [[maybe_unused]] constexpr auto operator~(const flags_t value) noexcept -> flags_t { \
-        return static_cast<flags_t>(~under_cast(value)); \
+    [[maybe_unused]] constexpr auto operator~(const enum_t value) noexcept -> enum_t { \
+        return static_cast<enum_t>(~under_cast(value)); \
     } \
-    [[maybe_unused]] constexpr auto operator&=(flags_t& lhs, const flags_t rhs) noexcept -> flags_t& { \
+    [[maybe_unused]] constexpr auto operator&=(enum_t& lhs, const enum_t rhs) noexcept -> enum_t& { \
         return lhs = lhs & rhs; \
     } \
-    [[maybe_unused]] constexpr auto operator|=(flags_t& lhs, const flags_t rhs) noexcept -> flags_t& { \
+    [[maybe_unused]] constexpr auto operator|=(enum_t& lhs, const enum_t rhs) noexcept -> enum_t& { \
         return lhs = lhs | rhs; \
     } \
-    [[maybe_unused]] constexpr auto operator^=(flags_t& lhs, const flags_t rhs) noexcept -> flags_t& { \
+    [[maybe_unused]] constexpr auto operator^=(enum_t& lhs, const enum_t rhs) noexcept -> enum_t& { \
         return lhs = lhs ^ rhs; \
     }
+
+template <typename... flag_ts>
+class flag_product {
+public:
+
+    constexpr flag_product() = default;
+
+    // TODO:HERE
+
+    template <typename rhs_flag_t>
+    friend constexpr auto operator|(const flag_product& lhs, const rhs_flag_t rhs) {
+
+    }
+
+    template <typename rhs_flag_ts...> // TODO:requires rhs_flag_ts to be a subset of flag_ts
+    friend constexpr auto operator|(const flag_product& lhs, const flag_product<rhs_flag_ts...>& rhs) {
+
+    }
+
+private:
+
+    std::tuple<flag_ts> m_flags;
+};
 
 #endif
--- test/test_flag_product.cpp
+++ test/test_flag_product.cpp
@@ -14,49 +14,49 @@ namespace {
     enum class config_1 { opt_1, opt_2 };
     enum class config_2 { opt_1, opt_2 };
 
-    enum class flags : std::uint8_t {
-        flag_1 = (1 << 0),
-        flag_2 = (1 << 1),
-        flag_3 = (1 << 2)
+    enum class bits : std::uint8_t {
+        bit_1 = (1 << 0),
+        bit_2 = (1 << 1),
+        bit_3 = (1 << 2)
     };
 
-    KSR_DEFINE_FLAGS(flags)
-    auto flag_domain = std::array{flags::flag_1, flags::flag_2, flags::flag_3};
-    using flag_domain_iter = decltype(flag_domain)::iterator;
+    KSR_DEFINE_BIT_OPERATIONS(bits)
+    auto bit_domain = std::array{bits::bit_1, bits::bit_2, bits::bit_3};
+    using bit_domain_iter = decltype(bit_domain)::iterator;
 
-    /// Given a range of `flags` codes describing a particular permutation of options, verifies that
-    /// combining those flags using various bitwise-or operations gives a compound value that can be
-    /// subsequently decomposed into each individual input flags (and only those input flags). More
-    /// formally, checks that the `|` and `|=` operators for `flags` behave in a consistent and
+    /// Given a range of `bits` codes describing a particular permutation of options, verifies that
+    /// combining those bits using various bitwise-or operations gives a compound value that can be
+    /// subsequently decomposed into each individual input bits (and only those input bits). More
+    /// formally, checks that the `|` and `|=` operators for `bits` behave in a consistent and
     /// commutative way, and that the `&` operator provides the inverse operation.
 
-    void check_permutation(const flag_domain_iter begin, const flag_domain_iter end) {
+    void check_bit_permutation(const bit_domain_iter begin, const bit_domain_iter end) {
 
         const auto rbegin = std::reverse_iterator{end};
         const auto rend = std::reverse_iterator{begin};
 
-        const auto folded_flags_l = std::accumulate(begin, end, flags{}, std::bit_or<flags>{});
-        const auto folded_flags_r = std::accumulate(rbegin, rend, flags{}, std::bit_or<flags>{});
-        REQUIRE(folded_flags_l == folded_flags_r);
+        const auto folded_bits_l = std::accumulate(begin, end, bits{}, std::bit_or<bits>{});
+        const auto folded_bits_r = std::accumulate(rbegin, rend, bits{}, std::bit_or<bits>{});
+        REQUIRE(folded_bits_l == folded_bits_r);
 
-        static const auto bit_or_assign = [](flags& value, const flags rhs) { value |= rhs; };
-        const auto mutated_flags_l = mutate_for_each(begin, end, flags{}, bit_or_assign);
-        const auto mutated_flags_r = mutate_for_each(rbegin, rend, flags{}, bit_or_assign);
-        REQUIRE(mutated_flags_l == mutated_flags_r);
-        REQUIRE(folded_flags_l == mutated_flags_l);
+        static const auto bit_or_assign = [](bits& value, const bits rhs) { value |= rhs; };
+        const auto mutated_bits_l = mutate_for_each(begin, end, bits{}, bit_or_assign);
+        const auto mutated_bits_r = mutate_for_each(rbegin, rend, bits{}, bit_or_assign);
+        REQUIRE(mutated_bits_l == mutated_bits_r);
+        REQUIRE(folded_bits_l == mutated_bits_l);
 
         // Note the use of REQUIRE() rather than CHECK() above: we have here confirmed that all
-        // methods of combining the flags for the current permutation are equivalent, and can
+        // methods of combining the bits for the current permutation are equivalent, and can
         // proceed by choosing any of those.
 
-        const auto& combined_flags = folded_flags_l;
-        for (auto&& flag : flag_domain) {
-            const auto has_flag = ((combined_flags & flag) == flag);
-            CHECK(has_flag == contains(begin, end, flag));
+        const auto& combined_bits = folded_bits_l;
+        for (auto&& bit : bit_domain) {
+            const auto has_bit = ((combined_bits & bit) == bit);
+            CHECK(has_bit == contains(begin, end, bit));
         }
     }
 }
 
-TEST_CASE("flag_operations", "[flag_product]") {
-    sub_permute(flag_domain, check_permutation);
+TEST_CASE("bit_operations", "[flag_product]") {
+    sub_permute(bit_domain, check_bit_permutation);
 }
